f908d86e7824af4c7ccc59eb27217567e4de06d9
Damian Strojek <67586060+damianStrojek@users.noreply.github.com>
2023-05-24 20:09:28 +0200

    Added power metrics and updated print functions, testing on kask

--- measure-performance.cpp
+++ measure-performance.cpp
@@ -21,12 +21,12 @@
 #include <termios.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <mpi.h>
+//#include <mpi.h>
 // Internal headers
 #include "metrics.h"
 #include "metrics-save.h"
 #include "metrics-display.h"
-#include "node-synchronization.h"
+//#include "node-synchronization.h"
 
 #define GPROCESSID 1				// PID of process that we are focused on (G stands for global)
 #define DATA_BATCH 10				// How many times you want to download metrics
@@ -48,7 +48,7 @@ int main(int argc, char **argv){
 	/*std::string fileName = timestamp += "_metrics.csv";
 	std::ofstream file(fileName, std::ios::out);
 	if(!file.is_open()) std::cerr << "\n\n\t [ERROR] Unable to open file " << fileName << " for writing.\n";*/
-	
+	/*
 	int rank, size;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -77,7 +77,7 @@ int main(int argc, char **argv){
 
 	MPI_Type_create_struct(6, blocklengths, offsets, types, &allMetricsType);
 	MPI_Type_commit(&allMetricsType);
-	AllMetrics* allMetricsArray = new AllMetrics[size];
+	AllMetrics* allMetricsArray = new AllMetrics[size];*/
 
 	// Download metrics in constant batches
 	for(int i = 0; i < DATA_BATCH; i++){
@@ -94,7 +94,7 @@ int main(int argc, char **argv){
 		getMemoryMetrics(memoryMetrics);
 		getNetworkMetrics(networkMetrics);
 		getPowerMetrics(powerMetrics, raplError, nvmlError);
-	
+	/*
 		if(rank)
 			MPI_Send(&allMetrics, 1, allMetricsType, 0, 0, MPI_COMM_WORLD);
 		else {
@@ -105,9 +105,9 @@ int main(int argc, char **argv){
 				std::cout << "\n\t[PROCESS " << i << " METRICS]\n";
 				printMetrics(&allMetricsArray[i].systemMetrics, &allMetricsArray[i].processorMetrics, \
 						&allMetricsArray[i].inputOutputMetrics, &allMetricsArray[i].memoryMetrics, \
-						&allMetricsArray[i].networkMetrics);
+						&allMetricsArray[i].networkMetrics, &allMetricsArray[i].powerMetrics);
 			}
-		}
+		}*/
 
 		sleep(2);
 		//auto end = std::chrono::high_resolution_clock::now();
@@ -115,12 +115,12 @@ int main(int argc, char **argv){
 		//std::cout << "Time taken to get all measures:" << duration.count() << "microseconds\n";
 
 		// Display metrics
-		//printMetrics(&systemMetrics, &processorMetrics, &inputOutputMetrics, &memoryMetrics, &networkMetrics);
+		printMetrics(&systemMetrics, &processorMetrics, &inputOutputMetrics, &memoryMetrics, &networkMetrics, &powerMetrics);
 
 		// Save metrics to file
 		//writeToCSV(file, timestamp, systemMetrics, processorMetrics, inputOutputMetrics, memoryMetrics, networkMetrics);
 	}
-
+	/*
 	//file.close();
 	MPI_Type_free(&systemMetricsType);
 	MPI_Type_free(&processorMetricsType);
@@ -130,6 +130,6 @@ int main(int argc, char **argv){
 	MPI_Type_free(&powerMetricsType);
 	MPI_Type_free(&allMetricsType);
 	delete[] allMetricsArray;
-   	MPI_Finalize();
+   	MPI_Finalize();*/
 	return 0;
 };
--- metrics-display.cpp
+++ metrics-display.cpp
@@ -15,34 +15,10 @@
 #include "metrics.h"
 #include "metrics-display.h"
 
-#define NOTSUPPORTED 666
+void printMetricPair(std::string metricName, float metricValue, std::string metricUnit, 
+			std::string metricNameTwo, float metricValueTwo, std::string metricUnitTwo){
 
-void printMetric(std::string metricName, int metricValue, std::string metricUnit){
-
-	std::string value = std::to_string(metricValue);
-	if(metricValue == NOTSUPPORTED) {
-		value = "notSupported";
-		metricUnit = "";
-	}
-	std::cout << metricName << std::right << std::setfill('.') << std::setw(30-metricName.length()) << value << metricUnit << "\n";
-
-    return;
-};
-
-void printMetricPair(std::string metricName, int metricValue, std::string metricUnit, 
-			std::string metricNameTwo, int metricValueTwo, std::string metricUnitTwo){
-
-	std::string value = std::to_string(metricValue);
-	if(metricValue == NOTSUPPORTED) {
-		value = "notSupported";
-		metricUnit = "";
-	}
-
-	std::string value2 = std::to_string(metricValueTwo);
-	if(metricValueTwo == NOTSUPPORTED) {
-		value2 = "notSupported";
-		metricUnitTwo = "";
-	}
+	std::string value = std::to_string(metricValue), value2 = std::to_string(metricValueTwo);
 
 	std::cout << metricName << std::right << std::setfill('.') << std::setw(30-metricName.length()) << value << metricUnit;
 	for(int i=0; i<20-metricUnit.length(); i++) std::cout << ' ';
@@ -54,7 +30,8 @@ void printMetricPair(std::string metricName, int metricValue, std::string metric
 };
 
 void printMetrics(SystemMetrics* systemMetrics, ProcessorMetrics* processorMetrics, 
-			InputOutputMetrics* inputOutputMetrics, MemoryMetrics* memoryMetrics, NetworkMetrics* networkMetrics){
+			InputOutputMetrics* inputOutputMetrics, MemoryMetrics* memoryMetrics, 
+			NetworkMetrics* networkMetrics, PowerMetrics* powerMetrics){
 
 	auto now = std::chrono::system_clock::now();
   	std::time_t now_c = std::chrono::system_clock::to_time_t(now);
@@ -64,28 +41,33 @@ void printMetrics(SystemMetrics* systemMetrics, ProcessorMetrics* processorMetri
 	for(int i=0;i<43;i++) std::cout << ' ';
 
 	std::cout << "Network:" << '\n';
-	printMetricPair("Processes running", systemMetrics->processesRunning,"", "Received packets",networkMetrics->receivedData,"");
-	printMetricPair("All Processes", systemMetrics->processesAll,"","Received packets rate", networkMetrics->receivePacketRate,"KB/s");
-	printMetricPair("Context Switch Rate", systemMetrics->contextSwitchRate,"/s","Sent packets",networkMetrics->sentData,"");
-	printMetricPair("Interrupt rate", systemMetrics->interruptRate,"/s","Sent packets rate",networkMetrics->sendPacketsRate,"KB/s");
+	printMetricPair("Running Processes", systemMetrics->processesRunning,"", "Received Packets",networkMetrics->receivedData,"");
+	printMetricPair("All Processes", systemMetrics->processesAll,"","Received Packets Rate", networkMetrics->receivePacketRate,"KB/s");
+	printMetricPair("Context Switch Rate", systemMetrics->contextSwitchRate,"/s","Sent Packets",networkMetrics->sentData,"");
+	printMetricPair("Interrupt Rate", systemMetrics->interruptRate,"/s","Sent Packets Rate",networkMetrics->sendPacketsRate,"KB/s");
 	std::cout << '\n';
 
 	std::cout << "Memory:";
 	for(int i=0;i<43;i++) std::cout << ' ';
 
 	std::cout << "Processor:" << '\n';
-	printMetricPair("Memory used", memoryMetrics->memoryUsed,"MB","Time user",processorMetrics->timeUser,"USER_HZ");
-	printMetricPair("Memory cached", memoryMetrics->memoryCached,"MB","Time system",processorMetrics->timeSystem,"USER_HZ");
-	printMetricPair("Swap used", memoryMetrics->swapUsed,"MB","Time idle",processorMetrics->timeIdle,"USER_HZ");
-	printMetricPair("Swap cached", memoryMetrics->swapCached,"MB","Time I/O wait",processorMetrics->timeIoWait,"USER_HZ");
+	printMetricPair("Memory Used", memoryMetrics->memoryUsed,"MB","Time User",processorMetrics->timeUser,"USER_HZ");
+	printMetricPair("Memory Cached", memoryMetrics->memoryCached,"MB","Time System",processorMetrics->timeSystem,"USER_HZ");
+	printMetricPair("Swap Used", memoryMetrics->swapUsed,"MB","Time Idle",processorMetrics->timeIdle,"USER_HZ");
+	printMetricPair("Swap Cached", memoryMetrics->swapCached,"MB","Time I/O Wait",processorMetrics->timeIoWait,"USER_HZ");
 	printMetricPair("Memory Active", memoryMetrics->memoryActive,"MB","Time IRQ",processorMetrics->timeIRQ,"USER_HZ");
 	printMetricPair("Memory Inactive", memoryMetrics->memoryInactive,"MB","Time Steal",processorMetrics->timeSteal,"USER_HZ");
+	printMetricPair("Pages Read", memoryMetrics->pageInRate, "pages/s", "Cache L2 Hit Rate", processorMetrics->cacheL2HitRate, "");
+	printMetricPair("Pages Saved", memoryMetrics->pageOutRate, "pages/s", "Cache L2 Miss Rate", processorMetrics->cacheL2MissRate, "");
+
+	std::cout << "Power:";
+	for(int i=0;i<43;i++) std::cout << ' ';
 
 	std::cout << '\n'<< "I/O for PID 1:" << '\n';
-	printMetric("Data Read ",inputOutputMetrics->dataRead,"MB");
-	printMetric("Data Written ",inputOutputMetrics->dataWritten,"MB");
-	printMetric("Read operations ",inputOutputMetrics->readOperationsRate,"");
-	printMetric("Write operations ",inputOutputMetrics->writeOperationsRate,"");
+	printMetricPair("Data Read ",inputOutputMetrics->dataRead,"MB", "Power for Cores", powerMetrics->coresPower, "W");
+	printMetricPair("Data Written ",inputOutputMetrics->dataWritten,"MB", "Processor Power", powerMetrics->processorPower, "W");
+	printMetricPair("Read Operations ",inputOutputMetrics->readOperationsRate,"/s", "Memory Power", powerMetrics->memoryPower, "W");
+	printMetricPair("Write Operations ",inputOutputMetrics->writeOperationsRate,"/s", "GPU Power", powerMetrics->gpuPower, "W");
 	std::cout << '\n';
 
     return;
