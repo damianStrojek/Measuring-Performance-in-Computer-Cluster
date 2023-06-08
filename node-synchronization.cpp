53a30d914673aa607b13a28da7334e573d29b387
Damian Strojek <67586060+damianStrojek@users.noreply.github.com>
2023-06-08 18:09:01 +0200

    Synchronization missing power metrics

--- node-synchronization.cpp
+++ node-synchronization.cpp
@@ -160,19 +160,27 @@ MPI_Datatype createMpiNetworkMetricsType(){
 // Create MPI data type for PowerMetrics
 MPI_Datatype createMpiPowerMetricsType(){
 
-    int blockLengths[] = {1, 1, 1, 1, 1};
+    int block_lengths[] = {
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
     MPI_Datatype types[] = {
-        MPI_FLOAT, MPI_FLOAT, MPI_FLOAT,
-        MPI_UNSIGNED_LONG_LONG, MPI_UNSIGNED_LONG_LONG};
+        MPI_FLOAT, MPI_FLOAT, MPI_FLOAT, MPI_FLOAT,
+        MPI_FLOAT, MPI_FLOAT, MPI_FLOAT, MPI_FLOAT,
+        MPI_FLOAT, MPI_FLOAT, MPI_FLOAT};
     MPI_Aint offsets[] = {
         offsetof(PowerMetrics, processorPower),
         offsetof(PowerMetrics, memoryPower),
         offsetof(PowerMetrics, systemPower),
         offsetof(PowerMetrics, gpuPower),
-        offsetof(PowerMetrics, gpuPowerHours)};
+        offsetof(PowerMetrics, gpuTemperature),
+        offsetof(PowerMetrics, gpuFanSpeed),
+        offsetof(PowerMetrics, gpuMemoryTotal),
+        offsetof(PowerMetrics, gpuMemoryUsed),
+        offsetof(PowerMetrics, gpuMemoryFree),
+        offsetof(PowerMetrics, gpuClocksCurrentSM),
+        offsetof(PowerMetrics, gpuClocksCurrentMemory)};
     
     MPI_Datatype powerMetricsType;
-    MPI_Type_create_struct(5, blockLengths, offsets, types, &powerMetricsType);
+    MPI_Type_create_struct(11, blockLengths, offsets, types, &powerMetricsType);
     MPI_Type_commit(&powerMetricsType);
 
     return powerMetricsType;
