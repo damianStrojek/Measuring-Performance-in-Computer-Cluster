c65e745c0a87d60b9f92253881408d11aa760bfc
dideek <piotrgarbowski2@gmail.com>
2023-06-08 16:24:44 +0200

    More fixes

--- measure-performance.cpp
+++ measure-performance.cpp
@@ -49,7 +49,8 @@ int main(int argc, char **argv){
 	//allMetrics.nodeTimestamp = exec(dateCommand);
 	//allMetrics.nodeTimestamp.pop_back();
 
-	std::string fileName = "results/" + exec(dateCommand) + "_metrics.json";
+	//std::string fileName = "results/" + exec(dateCommand) + "_metrics.json";
+	std::string fileName = "results/" +"test_metrics.json";
 	std::ofstream outputFile(fileName, std::ios::out);
 	if(!outputFile.is_open()) std::cerr << "\n\n\t [ERROR] Unable to open file " << fileName << " for writing.\n";
 	
@@ -115,7 +116,7 @@ int main(int argc, char **argv){
 		//auto end = std::chrono::high_resolution_clock::now();
 		//auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end-start);
 		//std::cout << "Time taken to get all measures:" << duration.count() << "microseconds\n";
-		outputFile << jsonArray.dump(4);
+		if(rank==0) outputFile << jsonArray.dump(4);
 		// Save metrics to file
 		//writeToJSON(outputFile, allMetrics);
 	}
