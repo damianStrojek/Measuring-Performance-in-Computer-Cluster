7124df6ae16c840288fbab00a9aa26790156d4fc
dideek <piotrgarbowski2@gmail.com>
2023-06-08 17:16:44 +0200

    Date fixes

--- measure-performance.cpp
+++ measure-performance.cpp
@@ -48,7 +48,8 @@ int main(int argc, char **argv){
 	const char* dateCommand = "date +'%d%m-%H%M'";
 	//allMetrics.nodeTimestamp = exec(dateCommand);
 	//allMetrics.nodeTimestamp.pop_back();
-	std::string fileName = "results/" + exec(dateCommand).pop_back() + "_metrics.json";
+	std:: string date = exec(dateCommand).pop_back();
+	std::string fileName = "results/" + date + "_metrics.json";
 	std::ofstream outputFile(fileName, std::ios::out);
 	if(!outputFile.is_open()) std::cerr << "\n\n\t [ERROR] Unable to open file " << fileName << " for writing.\n";
 	
